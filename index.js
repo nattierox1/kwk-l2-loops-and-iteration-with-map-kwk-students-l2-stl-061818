e52c6e8647e7ff13a057138de4c10b93331078fb
nattierox1 <nattierox1@gmail.com>
2018-06-21 18:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Code your solution in this file.
-let lowerCaseStudentNames = ['Adele', 'Beyonc', 'Lady', 'Madonna', 'Rihanna', 'Taylor'] ;
+let names = ['Adele', 'Beyonc', 'Lady', 'Madonna', 'Rihanna', 'Taylor'];
