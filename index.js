707a93d72ef3e1ac9b480481f11f7dd3f6680d90
nattierox1 <nattierox1@gmail.com>
2018-06-21 19:10:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file.
+let names = ['Adele', 'Beyonce', 'Lady', 'Madonna', 'Rihanna', 'Taylor'];
+
+
+function lowerThis(name) {
+  return name.toLowerCase();
+}
+
+function lowerCaseStudentNames(array) {
+  return array.map(lowerThis);
+}
+
+/*
+console.log(lowerCaseStudentNames());
+
+
+  
+  /*
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i];
+    return name.toLowerCase();
+  }
+  */
+  
+
+  function splitName(name) {
+    const studentFirst = name.split(" ")[0];
+    const studentLast = name.split(" ")[1];
+    
+    return { firstName : studentFirst, lastName : 
+    studentLast);
+  }
+  function nameToAttributes(list) {
+   return list.map(splitName);
+  }
+  
