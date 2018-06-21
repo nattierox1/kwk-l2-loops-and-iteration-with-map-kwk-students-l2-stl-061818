74a61d5f31b171915a5adbcc5b574bb5b87cd527
nattierox1 <nattierox1@gmail.com>
2018-06-21 18:23:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 let names = ['Adele', 'Beyonc', 'Lady', 'Madonna', 'Rihanna', 'Taylor'];
 
 function lowerCaseStudentNames() {
-  
+  for (let i = 0; i < names.length; i++) {
+    return names[0]
+  }
   
   /*
   let name = names[0];
