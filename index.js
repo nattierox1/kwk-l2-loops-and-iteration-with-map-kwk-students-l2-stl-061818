f3b214230ddf827c621ae4c045a0c2010d05d956
nattierox1 <nattierox1@gmail.com>
2018-06-21 18:27:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 let names = ['Adele', 'Beyonc', 'Lady', 'Madonna', 'Rihanna', 'Taylor'];
 
 function lowerCaseStudentNames() {
+  
+  names.map()
+  /*
   for (let i = 0; i < names.length; i++) {
     return names[i].toLowerCase();
   }
+  */
   
   /*
   let name = names[0];
